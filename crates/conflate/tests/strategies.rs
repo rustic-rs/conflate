6a44a39fa380ffeec77673e90e2d085b9301e572
simonsan <14062932+simonsan@users.noreply.github.com>
2024-10-02 16:10:20 +0200

    adapt test names
    Signed-off-by: simonsan <14062932+simonsan@users.noreply.github.com>

--- crates/conflate/tests/strategies.rs
+++ crates/conflate/tests/strategies.rs
@@ -240,7 +240,7 @@ mod btreemap {
     }
 
     #[test]
-    fn test_overwrite() {
+    fn test_append_or_overwrite() {
         #[derive(Debug, Merge, PartialEq)]
         struct S(#[merge(strategy = conflate::btreemap::append_or_overwrite)] BTreeMap<u8, u8>);
 
@@ -262,7 +262,7 @@ mod btreemap {
     }
 
     #[test]
-    fn test_ignore() {
+    fn test_append_or_ignore() {
         #[derive(Debug, Merge, PartialEq)]
         struct S(#[merge(strategy = conflate::btreemap::append_or_ignore)] BTreeMap<u8, u8>);
 
@@ -284,7 +284,7 @@ mod btreemap {
     }
 
     #[test]
-    fn test_recurse() {
+    fn test_append_or_recurse() {
         #[derive(Debug, Merge, PartialEq)]
         struct N(#[merge(strategy = conflate::num::saturating_add)] u8);
 
