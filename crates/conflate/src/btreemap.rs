eda71497212d3e75571faa849f1855feeaa61068
Alexander Weiss <alex@weissfam.de>
2024-10-03 09:57:08 +0200

    corrections

--- crates/conflate/src/btreemap.rs
+++ crates/conflate/src/btreemap.rs
@@ -1,34 +1,27 @@
-// SPDX-FileCopyrightText: 2020 Robin Krahl <robin.krahl@ireas.org>
-// SPDX-License-Identifier: Apache-2.0 or MIT
-
 //! Merge strategies for `BTreeMap`s.
 //!
 //! These strategies are only available if the `std` feature is enabled.
 
 use std::collections::BTreeMap;
-use std::hash::Hash;
 
 /// Append values, on conflict, overwrite elements of `left` with `right`.
 ///
 /// In other words, this gives precedence to `right`.
-pub fn append_or_overwrite<K: Eq + Hash + Ord, V>(
-    left: &mut BTreeMap<K, V>,
-    right: BTreeMap<K, V>,
-) {
+pub fn append_or_overwrite<K: Eq + Ord, V>(left: &mut BTreeMap<K, V>, right: BTreeMap<K, V>) {
     left.extend(right)
 }
 
 /// Append values, on conflict, ignore elements from `right`.
 ///
 /// In other words, this gives precedence to `left`.
-pub fn append_or_ignore<K: Eq + Hash + Ord, V>(left: &mut BTreeMap<K, V>, right: BTreeMap<K, V>) {
+pub fn append_or_ignore<K: Eq + Ord, V>(left: &mut BTreeMap<K, V>, right: BTreeMap<K, V>) {
     for (k, v) in right {
         left.entry(k).or_insert(v);
     }
 }
 
 /// Append values, on conflict, recursively merge the elements.
-pub fn append_or_recurse<K: Eq + Hash + Ord, V: crate::Merge>(
+pub fn append_or_recurse<K: Eq + Ord, V: crate::Merge>(
     left: &mut BTreeMap<K, V>,
     right: BTreeMap<K, V>,
 ) {
